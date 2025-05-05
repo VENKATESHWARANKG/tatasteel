3f51777cc13cc23150a0d735586783facd2b43a6
Sanjay <sanjay4sjy@gmail.com>
2025-05-05 11:01:15 +0530

    same as below

--- dealer_target.py
+++ dealer_target.py
@@ -134,7 +134,7 @@ st.title("Dealer Incentive Allocation")
 
 # ---- Load CSV File ----
 # load input file for incentive calculation
-input_data = pd.read_csv('Data/dealer_incentive_model_v2.csv')
+input_data = pd.read_csv('/root/tata/tatasteel/Data/dealer_incentive_model_v2.csv')
 # prev_data = pd.read_csv('Data/prev_oct_predicted_target.csv')
 
 
